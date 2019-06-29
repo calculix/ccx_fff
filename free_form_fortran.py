0d5e5c754ce86074537d52cb611c09d18baa7015
imirzov <imirzov@gmail.com>
2019-06-29 15:56:34 +0200

    none

--- free_form_fortran.py
+++ free_form_fortran.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-    © Ihor Mirzov, UJV Rez, June 2019.
+    © Ihor Mirzov, June 2019.
     Distributed under GNU General Public License, version 2.
 
     Converts old Calculix 2.15 Fortran 77 code to the one with free form.
